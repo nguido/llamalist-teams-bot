5907fa1e0408d0f96bab719c4c15ec9b1cd76265
nguido <nguido14@apu.edu>
2023-12-04 09:47:17 -0800

    a lot of stuff

--- message-extension.js
+++ message-extension.js
@@ -2,7 +2,7 @@ const { TeamsActivityHandler } = require('botbuilder')
 
 class MessageExtension extends TeamsActivityHandler {
     async handleTeamsMessagingExtensionFetchTask(context, action) {
-        const msUserId = context._activity.from.aadObjectId
+        // const msUserId = context._activity.from.aadObjectId
         const message = action.messagePayload
             ? action.messagePayload.body.content
             : ''
@@ -11,15 +11,15 @@ class MessageExtension extends TeamsActivityHandler {
             : ''
 
         const llamaExtensionEndpoint = encodeURI(
-            `https://a2a7-2600-6c52-7900-7b-215a-bd04-92e3-8cf5.ngrok-free.app/teamsExtension/?msUserId=${msUserId}&message=${message}&link=${link}`
+            `https://af7b-2600-6c52-7900-7b-8c1b-d9d7-a0ad-cb2b.ngrok-free.app/teams/message-extension?message=${message}&link=${link}`
         )
         if (message !== '' && link !== '') {
             return {
                 task: {
                     type: 'continue',
                     value: {
-                        width: 500,
-                        height: 400,
+                        width: 600,
+                        height: 200,
                         url: llamaExtensionEndpoint,
                         title: 'Create a Task in Llama Llist',
                     },
